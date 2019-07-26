790d378c72deecf285b3f45035e438691b7fd29f
Jeffrey <jeffrey@visual-trials.io>
2019-07-26 19:22:31 +0200

    removed gojs and mxgraph

--- sysview.js
+++ sysview.js
@@ -1,17 +1,12 @@
 let containersAndConnections = null
 
 function init() {
-   
-    // let graphContainerDiv = document.getElementById('sysViewDiv')
-    
-    // initMxGraph(graphContainerDiv);
-        
     
     // TODO: replace this eventually
     containersAndConnections = getExampleData()
     
-    initSelfMade()
-    
+    addInputListeners()
+    drawContainers()
 }
 
 function getExampleData() {
@@ -124,7 +119,7 @@ function addContainer(containerData, parentContainerIdentifier, containersToAddT
     return containerIdentifier
 }
 
-/* Self made */
+
 
 let canvasElement = document.getElementById('canvas')
 let ctx = canvasElement.getContext("2d")
@@ -135,11 +130,6 @@ let interaction = {
     viewIsBeingDragged : false,
     currentlySelectedContainer : null,
     selectedContainerIsBeingDragged : false
-}
-
-function initSelfMade() {
-    addInputListeners()
-    drawContainers()
 }
 
 function clearCanvas() {
@@ -387,178 +377,3 @@ function addInputListeners () {
     // TODO: for now preventing the context-menu this way
     canvasElement.addEventListener('contextmenu', event => event.preventDefault());
 }
-
-
-/* Go.js */
-
-// let $$ = go.GraphObject.make
-
-function initGoJS() {    
-    
-    let sysViewDiagram =
-        $$(go.Diagram, "sysViewDiv",
-            {
-                "undoManager.isEnabled": true,
-                "animationManager.isEnabled": false,
-            })
-
-    sysViewDiagram.groupTemplateMap.add("",
-        $$(go.Group, "Auto",
-            { 
-                fromSpot: go.Spot.Right, 
-                toSpot: go.Spot.Left,
-            },
-            { 
-                // TODO: we are using "PH" here. But does this exactly work?
-                selectionObjectName: "PH",
-                locationObjectName: "PH",
-                resizable: true,
-                resizeObjectName: "PH" 
-            },
-            { 
-                portId: "", 
-                fromLinkable: true, 
-                toLinkable: true, 
-                cursor: "pointer"
-            },
-            $$(go.Shape, "Rectangle",
-                { name: "PH" },
-                new go.Binding("stroke", "stroke"),
-                new go.Binding("fill", "fill"),
-                new go.Binding("desiredSize", "size", 
-                            function (size) { 
-                                return new go.Size(size.width, size.height) 
-                            }
-                        )
-                        .makeTwoWay(
-                            function (desiredSize) { 
-                                return { width: desiredSize.width, height: desiredSize.height }
-                            }
-                        ),
-            ),
-            new go.Binding("location", "position", 
-                            function (position) { 
-                                return new go.Point(position.x, position.y) 
-                            }
-                        )
-                        .makeTwoWay(
-                            function (location) { 
-                                return { x: location.x, y: location.y }
-                            }
-                        ),
-            $$(go.TextBlock,
-                { stroke: "white", margin: 3 },
-                new go.Binding("text", "key")
-            )
-        )
-    )
-    
-    /*
-    sysViewDiagram.toolManager.linkingTool.temporaryLink =
-        $$(go.Link,
-            { layerName: "Tool" },
-            $$(go.Shape,
-                { stroke: "red", strokeWidth: 2, strokeDashArray: [4, 2] })
-        )
-    
-    let tempFromNode =
-        $$(go.Node,
-            { layerName: "Tool" },
-            $$(go.Shape, "RoundedRectangle",
-                { stroke: "chartreuse", strokeWidth: 3, fill: null, portId: "", width: 1, height: 1 }
-            )
-        )
-    sysViewDiagram.toolManager.linkingTool.temporaryFromNode = tempFromNode;
-    sysViewDiagram.toolManager.linkingTool.temporaryFromPort = tempFromNode.port;
-    
-    let tempToNode =
-        $$(go.Node,
-            { layerName: "Tool" },
-            $$(go.Shape, "RoundedRectangle",
-                { stroke: "cyan", strokeWidth: 3, fill: null, portId: "", width: 1, height: 1 }
-            )
-        )
-    sysViewDiagram.toolManager.linkingTool.temporaryToNode = tempToNode
-    sysViewDiagram.toolManager.linkingTool.temporaryToPort = tempToNode.port
-    */
-    
-    
-    /*
-    sysViewDiagram.nodeTemplateMap.add("",
-        $$(go.Node, "Auto",
-            { 
-                fromSpot: go.Spot.Right, 
-                toSpot: go.Spot.Left
-            },
-            $$(go.Shape, "Rectangle",
-                new go.Binding("stroke", "stroke"),
-                new go.Binding("fill", "fill"),
-                // FIXME: this is old:
-                new go.Binding("width", "width"),
-                new go.Binding("height", "height")
-            ),
-            new go.Binding("location", "loc", function (loc) { return new go.Point(loc.x, loc.y) })
-        )
-    )
-    */
-    
-    // notice whenever a transaction or undo/redo has occurred
-    sysViewDiagram.addModelChangedListener(function(evt) {
-        if (evt.isTransactionFinished) {
-            console.log(evt.model.nodeDataArray)
-            // TODO: implement this: saveModel(evt.model)
-        }
-    })
-    
-    // TODO: replace this eventually
-    containersAndConnections = getExampleData()
-    loadDataIntoGraphModel(sysViewDiagram, containersAndConnections)
-    
-}
-
-function loadDataIntoGraphModel(sysViewDiagram) {
-    sysViewDiagram.clear()
-    sysViewDiagram.model = new go.GraphLinksModel(containersAndConnections.containers, containersAndConnections.connections)
-}
-
-/* mxGraph */
-
-function initMxGraph(containerDiv)
-{
-    // Checks if the browser is supported
-    if (!mxClient.isBrowserSupported())
-    {
-        // Displays an error message if the browser is not supported.
-        mxUtils.error('Browser is not supported!', 200, false)
-    }
-    else
-    {
-        // Disables the built-in context menu
-        mxEvent.disableContextMenu(containerDiv);
-        
-        // Creates the graph inside the given container
-        let graph = new mxGraph(containerDiv)
-
-        // Enables rubberband selection
-        new mxRubberband(graph)
-        
-        // Gets the default parent for inserting new cells. This
-        // is normally the first child of the root (ie. layer 0).
-        let parent = graph.getDefaultParent()
-                        
-        // Adds cells to the model in a single step
-        graph.getModel().beginUpdate()
-        try
-        {
-            let v1 = graph.insertVertex(parent, null, 'Hello,', 20, 20, 80, 30)
-            let v2 = graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30)
-            let e1 = graph.insertEdge(parent, null, '', v1, v2)
-        }
-        finally
-        {
-            // Updates the display
-            graph.getModel().endUpdate()
-        }
-    }
-}
-
