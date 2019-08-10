0b2037dbb2d5a70cd0cac01c8a5d22cf23b611e0
Jeffrey <jeffrey@visual-trials.io>
2019-08-10 14:53:44 +0200

    added fixme

--- interaction.js
+++ interaction.js
@@ -66,6 +66,9 @@ function handleInputStateChange () {
         
         mouseState.worldPosition = fromScreenPositionToWorldPosition(mouseState.position)
         
+        // FIXME: it seems that when you keep zooming in with the mouse pointer fixed on a point
+        //        the point is not really the point of zoom.
+        
         // FIXME: shouldnt we update all absolute positions? Since we changed viewScale and viewOffset!
     }
     
