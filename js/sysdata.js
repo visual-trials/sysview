ae70cbfef9f65861dade05181e0244bb153afa4f
Jeffrey <jeffrey@visual-trials.io>
2019-09-20 14:50:43 +0200

    allow 2 sources (hacky way) and very simple way of choosing which conversion

--- js/sysdata.js
+++ js/sysdata.js
@@ -22,25 +22,62 @@ function init() {
 
 // FIXME: hardcoded!
 let projectIdentifier = 'ClientLive'
-let sourceNamedAs = 'sysadmin'
-let sourceIdentifier = 'sources/client_live_sysadmin.json'
-let conversionIdentifier = 'conversions/convert_sysadmin.js'
-let destinationIdentifier = 'source.json'
+let sourceNamedAs = null
+let sourceIdentifier = null
+let sourceNamedAs2 = null
+let sourceIdentifier2 = null
+let conversionIdentifier = null
+let destinationIdentifier = null
+
+let do_sysadmin_convert = false
+let do_os_convert = false
+
+if (do_sysadmin_convert) {
+    projectIdentifier = 'ClientLive'
+    sourceNamedAs = 'sysadmin'
+    sourceIdentifier = 'sources/client_live_sysadmin.json'
+    conversionIdentifier = 'conversions/convert_sysadmin.js'
+    destinationIdentifier = 'sources/sysadmin_converted.json'
+}
+else if (do_os_convert) {
+    projectIdentifier = 'ClientLive'
+    sourceNamedAs = 'os'
+    sourceIdentifier = 'sources/client_live_os.json'
+    conversionIdentifier = 'conversions/convert_os.js'
+    destinationIdentifier = 'sources/os_converted.json'
+}
+else {
+    projectIdentifier = 'ClientLive'
+    sourceNamedAs = 'sysadmin'
+    sourceNamedAs2 = 'os'
+    sourceIdentifier = 'sources/client_live_os.json'
+    sourceIdentifier2 = 'sources/sysadmin_converted.json'
+    conversionIdentifier = 'conversions/combine_os_and_sysadmin.js'
+    destinationIdentifier = 'source.json'
+}
+
 
 function load() {
     loadSourceData(projectIdentifier, sourceIdentifier)  // ASYNC!
+    if (sourceIdentifier2 != null) {
+        loadSourceData2(projectIdentifier, sourceIdentifier2)  // ASYNC!
+    }
     loadConversionCode(projectIdentifier, conversionIdentifier)  // ASYNC!
 }
 
 function run() {
     // TODO: allow for multiple parameters/source into the conversion function!
     let sourceDataElement = document.getElementById('sourceData')
+    let sourceDataElement2 = document.getElementById('sourceData2')
     let destinationDataElement = document.getElementById('destinationData')
     let conversionCodeElement = document.getElementById('conversionCode')
     
     let conversionCode = conversionCodeElement.value
     let sources = {}
     sources[sourceNamedAs] = JSON.parse(sourceDataElement.value)
+    if (sourceNamedAs2 != null) {
+        sources[sourceNamedAs2] = JSON.parse(sourceDataElement2.value)
+    }
     let conversionFunction = new Function('sources', conversionCode)
     
     destinationDataElement.value = JSON.stringify(conversionFunction(sources), null, 4)
@@ -65,6 +102,22 @@ function loadSourceData(projectIdentifier, sourceIdentifier) {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             let sourceData = JSON.parse(xmlhttp.responseText)
             let sourceDataElement = document.getElementById('sourceData')
+            
+            sourceDataElement.value = JSON.stringify(sourceData.sourceData, null, 4)
+        }
+    }
+    xmlhttp.open("GET", url, true)
+    xmlhttp.send()
+}
+
+// FIXME: we should not duplicate this function!
+function loadSourceData2(projectIdentifier, sourceIdentifier) {
+    let url = 'index.php?action=get_source_data&project=' + projectIdentifier + '&source=' + sourceIdentifier
+    let xmlhttp = new XMLHttpRequest()
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            let sourceData = JSON.parse(xmlhttp.responseText)
+            let sourceDataElement = document.getElementById('sourceData2') // FIXME
             
             sourceDataElement.value = JSON.stringify(sourceData.sourceData, null, 4)
         }
