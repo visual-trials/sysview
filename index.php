b880fb3fa6e81942bc6f4db0b9db25f3ee659259
Jeffrey <jeffrey@visual-trials.io>
2019-11-28 19:43:29 +0100

    added ability to read csv files

--- index.php
+++ index.php
@@ -92,7 +92,20 @@ else if ($action === 'get_source_data') {
 
     $filenameSource = "projects/$projectDir/$sourceFile";
 
-    $sourceData = json_decode(file_get_contents($filenameSource), true);
+	$pathInfo = pathinfo($filenameSource);
+	if ($pathInfo["extension"] == "csv") {
+		// TODO: make  $delimiter, $enclosure, $escape configurable
+		$rows   = array_map(function($row) { return str_getcsv($row, "\t", '"', "\\"); }, file($filenameSource));
+		$header = array_shift($rows);
+		$sourceData = [];
+		foreach($rows as $row) {
+			$sourceData[] = array_combine($header, $row);
+		}
+	}
+	else {
+		// We assume json-content by default
+		$sourceData = json_decode(file_get_contents($filenameSource), true);
+	}
 
     echo json_encode([ 
         'sourceData' => $sourceData,
