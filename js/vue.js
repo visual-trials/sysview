064a19ec62d4fedf1218059b59aa729d09ead0f6
Jeffrey <jeffrey@visual-trials.io>
2020-01-03 09:44:27 +0100

    loading integration data

--- js/vue.js
+++ js/vue.js
@@ -2,39 +2,14 @@ let myVue = new Vue({
     el: '#app',
     data: {
         projectName: '',
-        applications : [],
-        conversionList : []
+        sourceData : {}
     },
     mounted () {
         let projectIdentifier = 'ClientLive'
-        let sourceIdentifier = 'sources/live_applications.csv'
+        let sourceIdentifier = 'sources/integration_db.json'
         loadSourceData(projectIdentifier, sourceIdentifier)
-        loadConversionTree(projectIdentifier)
     }
 })
-
-function loadConversionTree(projectIdentifier) {
-    let url = 'index.php?action=get_conversion_tree&project=' + projectIdentifier
-    let xmlhttp = new XMLHttpRequest()
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            let conversionTreeData = JSON.parse(xmlhttp.responseText)
-            let conversionList = []
-            for (conversionName in conversionTreeData.conversionTree) {
-                let conversion = conversionTreeData.conversionTree[conversionName]
-                conversion.name = conversionName
-
-                conversionList.push(conversion)
-            }
-            myVue.conversionList = conversionList
-            myVue.projectName = projectIdentifier
-            
-//console.log(myVue.conversionList)
-        }
-    }
-    xmlhttp.open("GET", url, true)
-    xmlhttp.send()
-}
 
 function loadSourceData(projectIdentifier, sourceIdentifier) {
     let url = 'index.php?action=get_source_data&project=' + projectIdentifier + '&source=' + sourceIdentifier
@@ -43,9 +18,8 @@ function loadSourceData(projectIdentifier, sourceIdentifier) {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             let sourceData = JSON.parse(xmlhttp.responseText)
             
-            // FIXME: hardcoded to 'applications'
-            myVue.applications = sourceData.sourceData
-            
+            myVue.sourceData = sourceData.sourceData // TODO: we probably dont want the data to be inside .sourceData
+            myVue.projectName = projectIdentifier
         }
     }
     xmlhttp.open("GET", url, true)
