2fafa3aad01f2eb1e0ef33622fada0d945199783
Jeffrey <jeffrey@visual-trials.io>
2020-01-16 09:12:56 +0100

    URL doesnt work in IE 11, so we are using a workaround

--- js/vue.js
+++ js/vue.js
@@ -26,9 +26,15 @@ databaseData.colorAndShapeMappings.dataTypeToColor = {}
 
 let project = 'PoC'
 
+/*
+// Note: URL doesnt work in IE 11, so we are using a workaround
 let urlString = window.location.href
 let url = new URL(urlString)
 let projectOverrule = url.searchParams.get("project")
+*/
+let projectOverrule = getQueryString("project")
+
+
 if (projectOverrule != null) {
 	project = projectOverrule
 }
@@ -360,4 +366,27 @@ function groupById (listWithIds) {
         elementsById[listElement.id] = listElement
     }
     return elementsById
+}
+
+// Taken from here: https://stackoverflow.com/questions/48447629/new-urllocation-href-doesnt-work-in-ie
+function getQueryString() {
+      var key = false, res = {}, itm = null;
+      // get the query string without the ?
+      var qs = location.search.substring(1);
+      // check for the key as an argument
+      if (arguments.length > 0 && arguments[0].length > 1)
+        key = arguments[0];
+      // make a regex pattern to grab key/value
+      var pattern = /([^&=]+)=([^&]*)/g;
+      // loop the items in the query string, either
+      // find a match to the argument, or build an object
+      // with key/value pairs
+      while (itm = pattern.exec(qs)) {
+        if (key !== false && decodeURIComponent(itm[1]) === key)
+          return decodeURIComponent(itm[2]);
+        else if (key === false)
+          res[decodeURIComponent(itm[1])] = decodeURIComponent(itm[2]);
+      }
+
+      return key === false ? res : null;
 }
