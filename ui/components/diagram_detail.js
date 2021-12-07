0eb7c714ac209a07d428c9d050dbdc289291ad3b
Jeffrey <jeffrey@visual-trials.io>
2021-12-07 11:01:57 +0100

    refactor components

---
+++ ui/components/diagram_detail.js
@@ -0,0 +1,107 @@
+let DiagramDetail = {
+    openDiagramDetailFunction: null,
+    closeDiagramDetailFunction: null,
+    diagramEditor : {
+        editedDiagram: null,
+    }
+}
+
+DiagramDetail.setEditedDiagramUsingOriginalDiagram = function (originalDiagram) {
+    
+    if (originalDiagram == null) {
+        DiagramDetail.diagramEditor.editedDiagram = null
+    }
+    else {
+        // TODO: right now, we clone the link! Only when we SAVE will it be copied back again!
+        DiagramDetail.diagramEditor.editedDiagram = JSON.parse(JSON.stringify(originalDiagram))
+    }
+}
+
+DiagramDetail.createNewDiagramAndOpenDetail = function() {
+            
+    let diagramEditor = DiagramDetail.diagramEditor
+    
+    let newDiagram = createNewDiagram()
+    DiagramDetail.setEditedDiagramUsingOriginalDiagram(newDiagram)
+    
+    diagramEditor.editedDiagram.isNewDiagram = true
+    
+    // Creating a new diagram id here and (if succesful) async calling open modal
+    DiagramDetail.generateNewDiagramIdAndOpenDetail(diagramEditor.editedDiagram)
+}
+
+DiagramDetail.removeDiagramAndCloseDetail = function (editedDiagram) {
+    let detachNodesAndLinksAttachedToDiagram = true
+    removeDiagram(editedDiagram, detachNodesAndLinksAttachedToDiagram)
+
+    DiagramLegendaLodSelector.selectDiagram('default')
+    
+    DiagramDetail.closeDiagramDetailFunction()
+}
+
+DiagramDetail.saveDiagramDetail = function (editedDiagram) {
+    let diagramsById = NLC.nodesAndLinksData.diagramsById
+    if (editedDiagram.id in diagramsById) {
+        let selectedDiagram = diagramsById[editedDiagram.id]
+        storeChangesBetweenDiagrams(selectedDiagram, editedDiagram) // ASYNC!
+
+        DiagramDetail.closeDiagramDetailFunction()
+    }
+    else {
+        
+        // Since editedDiagram.id does not yet exist, we assume this is a new diagram (and its id has already been generated). 
+        // So we add and store it as a new new diagram here.
+        storeNewDiagram(editedDiagram)
+        
+        // TODO: we probably want to auto-select the diagram here!
+        
+        // TODO: DiagramLegendaLodSelector.diagramSelector.selectedDiagramId =
+        
+        delete editedDiagram.isNewDiagram
+        
+        DiagramDetail.closeDiagramDetailFunction()
+    }
+}
+
+DiagramDetail.loadDiagramDetailsAndOpenDiagramDetail = function (diagramId) {
+    
+    // TODO: we are currently not adding _source information in the diagram_detail-*template*. So effectively we are not loading extra details (like _source) for this diagram yet. 
+    
+    function openDiagramDetail (loadedDiagramDetails) {
+        
+        let diagramsById = NLC.nodesAndLinksData.diagramsById
+        if (loadedDiagramDetails.id in diagramsById) {
+            let oldOriginalDiagram = diagramsById[diagramId]
+            
+            // FIXME: HOW DO WE REPLACE THE ORIGINAL DIAGRAM in the diagrams *list*!? Should we get rid of this *list* of diagrams?
+            // FIXME: this is a workaround to make sure the diagram in the diagrams-list is also updated with the new information
+            oldOriginalDiagram.name = loadedDiagramDetails.name
+            oldOriginalDiagram.identifier = loadedDiagramDetails.identifier
+            oldOriginalDiagram.level = loadedDiagramDetails.level
+            oldOriginalDiagram.projectUrl = loadedDiagramDetails.projectUrl
+            
+            // Overwriting the diagram in diagramsById
+            diagramsById[loadedDiagramDetails.id] = loadedDiagramDetails
+            DiagramDetail.setEditedDiagramUsingOriginalDiagram(loadedDiagramDetails)
+        }
+        else {
+            console.log("ERROR: the details of a diagram is reloaded, but it doesn't exist anymore in the diagramsById!?")
+        }
+        
+        DiagramDetail.openDiagramDetailFunction()
+    }
+    
+    let dataDetailsToLoad = [ 'diagrams' , diagramId ]
+    loadIkbDataDetails(dataDetailsToLoad, openDiagramDetail) // ASYNC!
+}
+
+DiagramDetail.generateNewDiagramIdAndOpenDetail = function (editedDiagram) {
+    
+    function assignIdToNewDiagramAndOpenModal(newId) {
+        editedDiagram.id = newId
+        
+        DiagramDetail.openDiagramDetailFunction()
+    }
+    generateNewId(assignIdToNewDiagramAndOpenModal) // ASYNC!
+}
+
---
+++ ui/components/user_management.js
@@ -0,0 +1,90 @@
+let UserManagement = {
+    knownUsersEditor: {
+        editedKnownUsers: null,
+    },
+    openKnownUsersFunction: null,
+    closeKnownUsersFunction: null,
+}
+
+UserManagement.setEditedKnownUsers = function (knownUsers) {
+    UserManagement.knownUsersEditor.editedKnownUsers = JSON.parse(JSON.stringify(knownUsers))
+}
+
+// Authorization
+
+UserManagement.userAuthorization = {
+    currentUser : null,
+    isRootUser : false
+}
+
+UserManagement.saveKnownUsers = function (editedKnownUsers) {
+    storeChangesBetweenKnownUsers(NLC.nodesAndLinksData.knownUsers, editedKnownUsers) // ASYNC!
+    UserManagement.closeKnownUsersFunction()
+}
+
+UserManagement.isAllowedTo = function(action) {
+    let currentUser = UserManagement.userAuthorization.currentUser
+    
+    if (UserManagement.userAuthorization.isRootUser) {
+        return true
+    }
+    
+    if (action === 'edit') {
+        if (currentUser && currentUser.userPermissions && currentUser.userPermissions.isAdmin) {
+            return true
+        }
+    }
+    return false
+}
+
+UserManagement.getUserInitials = function() {
+    let initials = null
+    
+    let currentUser = UserManagement.userAuthorization.currentUser
+    if (currentUser && 'concatName' in currentUser) {
+        let nameParts = currentUser.concatName.split(', ')
+        if (nameParts.length == 2) {
+            initials = nameParts[1].substring(0,1).toUpperCase() + nameParts[0].substring(0,1).toUpperCase()
+        }
+        else {
+            initials = currentUser.concatName.substring(0,2).toUpperCase()
+        }
+    }
+    return initials
+}
+
+UserManagement.getUserLogin = function() {
+    let login = null
+    
+    let currentUser = UserManagement.userAuthorization.currentUser
+    if (currentUser && 'login' in currentUser) {
+        login = currentUser.login
+    }
+    return login
+}
+
+UserManagement.getUserFullName = function() {
+    let fullName = null
+    
+    let currentUser = UserManagement.userAuthorization.currentUser
+    if (currentUser && 'concatName' in currentUser) {
+        let nameParts = currentUser.concatName.split(', ')
+        if (nameParts.length == 2) {
+            fullName = nameParts[1] + ' ' + nameParts[0]
+        }
+        else {
+            fullName = currentUser.concatName
+        }
+    }
+    return fullName
+}
+
+UserManagement.getUserTeamId = function() {
+    let teamId = null
+    
+    let currentUser = UserManagement.userAuthorization.currentUser
+    if (currentUser && 'teamId' in currentUser) {
+        teamId = currentUser.teamId
+    }
+    return teamId
+}
